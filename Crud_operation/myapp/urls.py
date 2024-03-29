87eac2820173e93fb7cad5da4fe2014c46eccb39
dev_krissh-at-629937844728 <krishnamitws2000@gmail.com>
2024-03-29 07:27:27 +0530

    Add & Update are done

--- Crud_operation/myapp/urls.py
+++ Crud_operation/myapp/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views import member
+from .views import add
+from .views import edit
 
 urlpatterns = [
     path("", member, name="home"),
+    path('add', add, name='add'),
+    path('edit', edit, name='edit'),
 ]
--- Crud_operation/myapp/views.py
+++ Crud_operation/myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from myapp.models import Employees
 
@@ -9,3 +9,30 @@ def member(request):
         'emp':emp,
     }
     return render(request, 'index.html',context)
+
+
+def add(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email =  request.POST.get('email')
+        address = request.POST.get('address')
+        phone = request.POST.get('phone')
+
+        emp = Employees(
+            name = name,
+            email = email,
+            address = address,
+            phone = phone
+        
+        )
+        emp.save()
+        return redirect('home')
+    
+    return render(request, 'index.html')
+
+def edit(request):
+    emp = Employees.objects.all()
+    context = {
+        'emp':emp,
+    }
+    return redirect(request,'index.html',context)
