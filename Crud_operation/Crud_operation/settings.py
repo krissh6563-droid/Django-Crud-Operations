a8cd01345c825f668c7836e978f950e954cf4df6
dev_krissh-at-629937844728 <krishnamitws2000@gmail.com>
2024-04-01 10:30:16 +0530

    Update

--- Crud_operation/Crud_operation/settings.py
+++ Crud_operation/Crud_operation/settings.py
@@ -75,7 +75,12 @@ WSGI_APPLICATION = 'Crud_operation.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
